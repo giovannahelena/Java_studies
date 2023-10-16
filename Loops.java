3beb725358d459d5c24ffecdededb913231eb7fb
Giovanna Helena <giovannahelenas@gmail.com>
2023-10-16 20:46:20 -0300

    Loops

---
+++ Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++)
+		{
+			System.out.println(i + ":");
+			for (int j = 1; j <= 10; j++)
+				System.out.println(i * j);
+			System.out.println("\n");
+		}
+	}
+}
