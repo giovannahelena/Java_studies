56b6541b487b9be14e48db18215755aa51840c49
Giovanna Helena <giovannahelenas@gmail.com>
2023-10-16 20:20:39 -0300

    manipulating date and time

--- DatesAndStrings.java
+++ DatesAndStrings.java
@@ -1,15 +1,25 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
 public class DatesAndStrings {
 			public static void main(String[] args) {
 				// Hello, {name}. Today is {day-of-week}. HAVE A NICE DAY! 
+				// Locale english = new Locale.Builder().setLanguage("en").setRegion("UK").build();
 
 				String name = "Helena";
+				String message = "have a great day!";
 				LocalDate today = LocalDate.now();
-				Locale spanish = new Locale.Builder().setLanguage("es").setRegion("AR").build();
-				System.out.println("Hello, " + name + ". Today is a " + today.getDayOfWeek().getDisplayName(TextStyle.FULL, spanish)
-				+ ".");
+				LocalDateTime now = LocalDateTime.now();
+				if (now.getHour() >= 4 && now.getHour() < 12)
+				message = "have a nice day!";
+				else if (now.getHour() >= 12 && now.getHour() < 19)
+				message = "good afternoon!";
+				else if (now.getHour() >= 19 && now.getHour() < 23)
+				message = "good evening!";
+				else if (now.getHour() >= 23 && now.getHour() < 4)
+				message = "good night.";
+				System.out.printf("Hello, %s. Today is a %s. %s%n", name, today.getDayOfWeek(), message.toUpperCase());
 			}
 }
